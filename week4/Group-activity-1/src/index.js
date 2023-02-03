0185aae012576186dcbf3a6fe1169969477a38d7
Arber <zarberleo@gmail.com>
2023-02-03 17:35:45 +0200

    group acitivity 3

--- week4/Group-activity-1/src/index.js
+++ week4/Group-activity-1/src/index.js
@@ -1,1 +1,9 @@
-//Your code here
+const foo = fetch('https://xkcd.now.sh/?comic=latest')
+  .then((response) => response.json())
+.then((data) => {
+    console.log(data);
+    const xy = document.createElement("img");
+    xy.setAttribute('alt', data.alt);
+    xy.setAttribute('src', data.img);
+    document.body.appendChild(xy);
+});
